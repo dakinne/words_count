9b44265fe2e00093a734ccf1a3a63efc1069306b
pedro <pedro@chicfy.com>
2019-12-07 18:59:42 +0100

    Add choose file and read it

--- app/src/main/java/com/noox/wordscount/words/ui/MainActivity.kt
+++ app/src/main/java/com/noox/wordscount/words/ui/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.noox.wordscount.words.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.DividerItemDecoration.VERTICAL
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,9 +18,13 @@ import com.noox.wordscount.R
 import com.noox.wordscount.databinding.ActivityMainBinding
 import com.noox.wordscount.words.ui.WordsAdapter.SortType.*
 import initBinding
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+
 
 // TODO: Ahora usamos coroutinas en la Activity, luego se usaran desde el ViewModel y la
 // Activity observara la palabras
@@ -57,7 +67,82 @@ class MainActivity : AppCompatActivity(), CoroutineScope by CoroutineScope(Dispa
             adapter.sortBy(Appearance)
             true
         }
+        R.id.menu_file -> {
+            if (permissionsAllowed()) { chooseFile() }
+            true
+        }
         else -> super.onOptionsItemSelected(item)
+    }
+
+    private fun permissionsAllowed(): Boolean {
+        if (checkPermissions()) {
+            return true
+        }
+
+        requestPermissions()
+        return false
+    }
+
+    private fun checkPermissions(): Boolean {
+        val permissionRequest = ActivityCompat.checkSelfPermission(this, READ_EXTERNAL_STORAGE)
+        return (permissionRequest == PERMISSION_GRANTED)
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this, arrayOf(READ_EXTERNAL_STORAGE), 1)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if(grantResults.isNotEmpty()) {
+            var allAllowed = true
+            grantResults.forEach {
+                if (it != PERMISSION_GRANTED) {
+                    allAllowed = false
+                }
+            }
+            if (allAllowed) {
+                chooseFile()
+            }
+        }
+    }
+
+    private fun chooseFile() {
+        val intent = Intent(ACTION_GET_CONTENT).apply {
+            type = "text/plain"
+        }
+        startActivityForResult(Intent.createChooser(intent, "Selecciona un fichero de texto"), RC_FILE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode != RC_FILE || resultCode != RESULT_OK) {
+            return
+        }
+
+        data?.data?.let {
+            importFile(it)
+        }
+    }
+
+    private fun importFile(uri: Uri) {
+
+        val byWord = "\\s+".toRegex()
+
+        val inputStream = contentResolver.openInputStream(uri) ?: return
+        val reader = BufferedReader(inputStream.reader())
+
+        launch {
+            flow {
+                reader.useLines { lines ->
+                    lines.forEach { line ->
+                        line.split(byWord).forEach { word ->
+                            emit(word)
+                        }
+                    }
+                }
+            }.collect {
+                Log.i("NOOX", "collect $it")
+            }
+        }
     }
 
     private fun words() = flow {
@@ -65,6 +150,10 @@ class MainActivity : AppCompatActivity(), CoroutineScope by CoroutineScope(Dispa
         words.forEach {
             emit(it)
         }
+    }
+
+    companion object {
+        private const val RC_FILE = 1
     }
 
 }
